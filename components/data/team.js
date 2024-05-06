126d477367a169d48a305844c9b19a615e672f05
Prithi <prithijack@gmail.com>
2024-05-06 13:34:34 +0530

    update

--- components/data/team.js
+++ components/data/team.js
@@ -1,40 +1,76 @@
-import team6 from "@/public/img/team/team-10.jpg";
-import team1 from "@/public/img/team/team-5.jpg";
-import team2 from "@/public/img/team/team-6.jpg";
-import team3 from "@/public/img/team/team-7.jpg";
-import team4 from "@/public/img/team/team-8.jpg";
-import team5 from "@/public/img/team/team-9.jpg";
+// import team6 from "@/public/img/team/team-10.jpg";
+// import team1 from "@/public/img/team/team-5.jpg";
+// import team2 from "@/public/img/team/team-6.jpg";
+// import team3 from "@/public/img/team/team-7.jpg";
+// import team4 from "@/public/img/team/team-8.jpg";
+// import team5 from "@/public/img/team/team-9.jpg";
+
+// const teams = [
+//     {
+//         name: "Michelle yeoh",
+//         status: "Senior Member",
+//         img: team1
+//     },
+//     {
+//         name: "James Damian",
+//         status: "Senior Member",
+//         img: team2
+//     },
+//     {
+//         name: "Davinci Resolve",
+//         status: "Senior Member",
+//         img: team3
+//     },
+//     {
+//         name: "David Fincher",
+//         status: "Senior Member",
+//         img: team4
+//     },
+//     {
+//         name: "Michelle Reis",
+//         status: "Senior Member",
+//         img: team5
+//     },
+//     {
+//         name: "Amelia Jessica",
+//         status: "Senior Member",
+//         img: team6
+//     },
+// ]
+
+// export default teams;
+
 
 const teams = [
     {
         name: "Michelle yeoh",
         status: "Senior Member",
-        img: team1
+        img: "../../public/img/team/team-5.jpg"
     },
     {
         name: "James Damian",
         status: "Senior Member",
-        img: team2
+        img: "../../public/img/team/team-6.jpg"
     },
     {
         name: "Davinci Resolve",
         status: "Senior Member",
-        img: team3
+        img: "../../public/img/team/team-7.jpg"
     },
     {
         name: "David Fincher",
         status: "Senior Member",
-        img: team4
+        img: "../../public/img/team/team-8.jpg"
     },
     {
         name: "Michelle Reis",
         status: "Senior Member",
-        img: team5
+        img: "../../public/img/team/team-9.jpg"
     },
     {
         name: "Amelia Jessica",
         status: "Senior Member",
-        img: team6
+        img: "../../public/img/team/team-10.jpg"
     },
 ]
 
